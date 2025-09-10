cbd32ccee2f7f550b6178f378e9c27206d1c3ea4
Penny <pennyspice@mailfence.com>
2025-09-10 19:38:54 -0400

    Create init.lua
    Added a simple neovim config

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.number = false
+vim.opt.relativenumber = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>w', ":w<CR>")
